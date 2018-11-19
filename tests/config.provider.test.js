c0d856253bf8b7d41c60c038c49575bb17cc4cd9
btoles <brennan.toles@gmail.com>
2018-11-19 15:22:24 -0600

    test(jest): Adding Jest and Configuration Provider tests

---
+++ tests/config.provider.test.js
@@ -0,0 +1,37 @@
+const Configuration = require('../src/providers/config.provider');
+const { DEFAULTS, CONFIG_FILE } = require('../src/utils/constants');
+const path = require('path');
+const fs = require('fs');
+const configPath = path.resolve(__dirname, CONFIG_FILE);
+
+
+describe('Configuration', () => {
+    describe('Order of Precedence', () => {
+        describe('CLI Arguments', () => {
+            it('should override config and defaults', () => {
+                const branch = 'args-branch';
+                const config = new Configuration({branch});
+                expect(config.branch()).toBe(branch);
+            });
+        });
+        describe('No CLI Arguments', () => {
+            it('should load config file values', () => {
+                const config = new Configuration({}, configPath);
+                expect(config.branch()).toBe('test-branch');
+                expect(JSON.stringify(config.mapping())).toBe(JSON.stringify({
+                    BREAKING: 'major',
+                    feature: 'minor',
+                    fix: 'patch'                    
+                }));
+            });
+        });
+        describe('No CLI, No Config File', () => {
+            it('should load default values', () => {
+                fs.existsSync = jest.fn(() => false);
+                const config = new Configuration();
+                expect(config.branch()).toBe(DEFAULTS.BRANCH);
+                expect(JSON.stringify(config.mapping())).toBe(JSON.stringify(DEFAULTS.RELEASE));
+            })
+        });
+    });
+});
